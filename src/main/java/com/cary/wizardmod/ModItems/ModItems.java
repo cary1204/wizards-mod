3d9bc9fed648028eba6165492b165bd215a273e5
Cary <homrwork.cary@gmail.com>
2025-06-05 22:09:41 -0400

    test  items pt2

---
+++ src/main/java/com/cary/wizardmod/ModItems/ModItems.java
@@ -0,0 +1,31 @@
+package com.cary.wizardmod.ModItems;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import com.cary.wizardmod.WizardsMod;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static final Item WIZARD_ITEM = registerItem("wizard_item", Item::new, new Item.Settings());
+
+    public static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registerKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(WizardsMod.MOD_ID, name));
+        return Items.register(registerKey, factory, settings);
+    }
+
+    private static void customIngredients(FabricItemGroupEntries entries) {
+        entries.add(WIZARD_ITEM);
+    }
+
+    public static void registerModItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::customIngredients);
+    }
+}
