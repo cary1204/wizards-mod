a0b6b3edd3d79b20e44b13b6f3b9527d64810659
Cary <homrwork.cary@gmail.com>
2025-06-05 21:45:48 -0400

    iniitaial

---
+++ src/main/java/com/cary/wizardmod/WizardsModClient.java
@@ -0,0 +1,10 @@
+package com.cary.wizardmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WizardsModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/cary/wizardmod/WizardsModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.cary.wizardmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WizardsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
